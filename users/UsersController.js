65f31c19af0f8b4e8dddf7729025ef9ec5522fd9
cesar-severo-programador <amiguinho.c@gmail.com>
2025-04-13 12:50:02 -0300

    added user creation with hashed password, added users page

--- users/UsersController.js
+++ users/UsersController.js
@@ -5,7 +5,9 @@ const bcrypt = require('bcryptjs');
 
 
 router.get("/admin/users", (req, res) => {
-    res.send("Rota de usuarios");
+    User.findAll().then(users => {
+        res.render("admin/users/index", {users: users});
+    });
 });
 
 
@@ -13,11 +15,33 @@ router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create");
 });
 
+
+//criando usuario
 router.post("/users/create", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    res.json({email, password});
+    //validando se o email ja existe
+    User.findOne({where: { email: email}}).then( user => {
+        if(user == undefined){
+
+                //criptografando a senha
+                var salt = bcrypt.genSaltSync(10);
+                var hash = bcrypt.hashSync(password, salt);
+
+                User.create({
+                    email: email,
+                    password: hash
+                }).then(() => {
+                    res.redirect("/");
+                }).catch((err) => {
+                    res.redirect("/");
+                });
+        }else{
+            res.redirect("/admin/users/create");
+        }
+    });
+
 });
 
 
